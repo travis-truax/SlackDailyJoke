117b0ae5752b0f02deddb4376e827730a0186362
Brendan Beck <brendan.beck62@gmail.com>
2023-02-27 20:33:19 -0800

    piost to cahnnel

--- src/handler.py
+++ src/handler.py
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
         return "no new comic"
     
     # send to slack
-    slack_response = slack.post_to_slack(comic_data)
+    slack_response = slack.post_to_channel(comic_data)
     if slack_response.status_code != 200:
         logger.error("Unable to post to Slack")
         slack_response.raise_for_status()
